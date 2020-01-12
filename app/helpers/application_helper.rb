133d9cdb218b2524030f0edbe63cd3a481fb911b
Akiko <shantianm514@gmail.com>
2020-01-12 23:57:32 +0000

    Finish Top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_name="") # メソッドと引数の定義
+    base_title = "AttendanceApp" #　基本となっているアプリケーション名を変数に代入
+    if page_name.empty? #　引数page_nameを受け取っているか判定
+      base_title #　引数が空の時はbase_titleだけを返す
+    else         # 引数が空でない場合は
+      page_name + "|" + base_title #　文字列を連結して返す
+    end
+  end  
+    
 end
